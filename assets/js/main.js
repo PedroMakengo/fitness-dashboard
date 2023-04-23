558153067b73eef363efcd27d3b8075079183b27
PedroMakengo <pedromakengomiguel@gmail.com>
2023-04-23 06:27:25 +0100

    Trabalhando e aprendendo sobre flexbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,9 @@ ScrollReveal({
   distance: '30px',
   duration: 700,
 }).reveal('.content-cards, .chart-two ,.chart-one')
+
+const loading = document.querySelector('.loading')
+
+setTimeout(() => {
+  loading.remove()
+}, 3000)
